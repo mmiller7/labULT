9c5e92d82870ec99dbf8cfd12c70f453d810ddb9
Matt <mmiller7@mail.umw.edu>
2011-10-23 17:43:37 -0400

    Crashing inside stub - new thread kinda works

--- ULT.c
+++ ULT.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include<stdlib.h>
 #include<stdio.h>
+#include<string.h>
 
 /* We want the extra information from these definitions */
 #ifndef __USE_GNU
@@ -18,14 +19,66 @@ Tid nextTid = 1;
 
 Tid ULT_CreateThread(void (*fn)(void *), void *parg)
 {
+	if(debug) printf("%s","Creating new thread\n");
+	
+	if(nextTid>ULT_MAX_THREADS)
+		return ULT_NOMORE;
+	//TODO put something to return ULT_NOMEMORY
+
+
   assert(1); /* TBD */
-	//TODO need to create a tbc somewhere in here and stick the new thing in it
-	//TODO assign it the next tid and increment nextTid
-	//TODO put the tcb in the queue
-	//TODO figure out what else goes here...new stack stuff?
-	stub(fn,parg);
-	//TODO figure out what to return
-  return ULT_FAILED;
+	//need to create a tbc somewhere in here and stick the new thing in it
+	if(debug)printf("%s","Creating a new tcb\n");
+	struct ThrdCtlBlk *tcb =  malloc(sizeof(struct ThrdCtlBlk));
+
+	//Allocate space for a new stack
+	if(debug)printf("%s","Creating a new stack\n");
+	void *stack = (void *) malloc(ULT_MIN_STACK);	
+	//move pointer to top
+	stack=stack+ULT_MIN_STACK;
+	//stub(fn,parg);
+
+	//push stuff on
+	if(debug)printf("%s","Making room for parg\n");
+	stack=stack-4;
+	if(debug)printf("%s","Pushing parg\n");
+	strcpy((char*)stack,parg);
+	if(debug)printf("%s","Making room for fn\n");
+	stack=stack-4;
+	if(debug)printf("%s","Pushing fn\n");
+	strcpy(stack,(char*)fn);
+
+	//get a context so we have something to mess with
+	if(debug)printf("%s","getting context\n");
+	getcontext(&tcb->context);
+
+	//change stack pointer to point to new stack
+	if(debug)printf("%s","point stack pointer to new stack\n");
+	tcb->context.uc_stack.ss_sp=stack;
+
+	//change EIP to point to stub
+	if(debug)printf("%s","changing EIP to point to stub function\n");
+	tcb->context.uc_mcontext.gregs[REG_EIP]=(int)stub;
+
+
+	//assign it the next tid and increment nextTid
+	tcb->tid=nextTid;
+	nextTid++;
+	//Locate end of queue
+
+	struct ThrdCtlBlk *end = phead;
+	while((end != NULL) && (end->next != NULL))
+		end=end->next;
+
+	//store the new tcb struct in the queue
+	if(debug)printf("%s","Adding it to queue\n");
+	if(end == NULL)
+		phead=tcb;
+	else
+		end->next=tcb;
+
+	if(debug) printf("%s%d%s","Created new thread ",tcb->tid,"\n");
+	return tcb->tid;
 }
 
 
@@ -97,19 +150,38 @@ Tid ULT_Yield(Tid wantTid)
 
 			//found will be the tcb for the new thread
 
-			//do the IP jump for what we're about to load
-			//needed to skip the load instruction and prevent infinite loop
-			//TODO
-			assert(0);
-
 			//make tcb for currently running thread
 			struct ThrdCtlBlk *tcb =  malloc(sizeof(struct ThrdCtlBlk));
+			tcb->tid=currentTid;
+			if(debug) printf("%s","Made a tcb\n");
+
+			//Locate end of queue
+			struct ThrdCtlBlk *end = phead;
+			while((end != NULL) && (end->next != NULL))
+				end=end->next;
+			if(debug) printf("%s","Found end of queue\n");
+
+			//store the new tcb struct in the queue
+			if(end == NULL)
+				phead=tcb;
+			else
+				end->next=tcb;
+			if(debug) printf("%s","Stored tcb in queue\n");
 
 			//save the currently running thread
 			getcontext(&tcb->context);
+			if(debug) printf("%s","Context gotted\n");
+
+			//do the IP jump for what we're about to load
+			//needed to skip the load instruction and prevent infinite loop
+			tcb->context.uc_mcontext.gregs[REG_EIP]=tcb->context.uc_mcontext.gregs[REG_EIP]+76;
+			if(debug) printf("%s","jumped\n");
 
 			//set the new context
 			setcontext(&found->context);
+			if(debug) printf("%s","Context setted\n");
+
+			return currentTid;
 		}
 		else
 		{
@@ -133,6 +205,8 @@ Tid ULT_DestroyThread(Tid tid)
 
 void stub(void (*root)(void *), void *arg)
 {
+		if(debug) printf("%s","In the stub\n");
+		//if(debug) printf("%s%s%s","Running stub passed arg ",(char)arg,"\n");
     // thread starts here
 		Tid ret;
 	  root(arg); // call root function
